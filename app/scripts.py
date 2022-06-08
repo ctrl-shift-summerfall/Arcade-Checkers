ad050c3116c37e57fa7d0cb893589a2124365541
Andrey Vostokov <52819269+ctrl-shift-summerfall@users.noreply.github.com>
2022-06-08 18:09:37 +0800

    #3 Conversion scripts
    1. Updated settings.py, added BOARD_ related settings and CHECKER_ related settings to variables.
    2. Updated scripts.py, added scripts that shall convert [list] coordinates to [list] grid position and vice versa:
    * Grid position is represented by a list containing a row and a column of a checker (eg. [0, 1], or B1 as 0 row is the first row on the plane and is "1", and 1 is the second column on the plane and is "B").
    * Coordinates are represented by a list containing coordinates x and y.

--- app/scripts.py
+++ app/scripts.py
@@ -1,18 +1,36 @@
 # Internal scripts and modules:
-from app.settings import GRID_NUMERIC_INDEX_STRING, GRID_ALPHABETIC_INDEX_STRING
+from app.settings import (GRID_NUMERIC_INDEX_STRING, GRID_ALPHABETIC_INDEX_STRING,
+                          BOARD_LINE_TOTAL_WIDTH, BOARD_TILE_SIZE_PX, BOARD_PLAY_SURFACE_MIN, BOARD_PLAY_SURFACE_MAX)
 
 
-def convert_grid_position_to_alphanumeric_position(grid_position: list):
-    row, column = grid_position
+def convert_grid_position_to_alphanumeric_position(conv_grid_position: list):
+    row, column = conv_grid_position
     numeric_index = GRID_NUMERIC_INDEX_STRING[row]
     alphabetic_index = GRID_ALPHABETIC_INDEX_STRING[column]
     alphanumeric_position = f'{alphabetic_index}{numeric_index}'
     return alphanumeric_position
 
 
-def convert_alphanumeric_position_to_grid_position(alphanumeric_position: str):
-    alphabetic_index, numeric_index = alphanumeric_position[0], alphanumeric_position[1]
+def convert_alphanumeric_position_to_grid_position(conv_alphanumeric_position: str):
+    alphabetic_index, numeric_index = conv_alphanumeric_position[0], conv_alphanumeric_position[1]
     row = GRID_NUMERIC_INDEX_STRING.index(numeric_index)
     column = GRID_ALPHABETIC_INDEX_STRING.index(alphabetic_index)
     grid_position = [row, column]
     return grid_position
+
+def convert_coordinates_to_grid_position(conv_coordinates: list):
+    grid_position = None
+    coordinate_x, coordinate_y = conv_coordinates
+    if bool(BOARD_PLAY_SURFACE_MAX > coordinate_x > BOARD_PLAY_SURFACE_MIN and
+            BOARD_PLAY_SURFACE_MAX > coordinate_y > BOARD_PLAY_SURFACE_MIN):
+        row = int((coordinate_x - BOARD_LINE_TOTAL_WIDTH) // BOARD_TILE_SIZE_PX)
+        column = int((coordinate_y - BOARD_LINE_TOTAL_WIDTH) // BOARD_TILE_SIZE_PX)
+        grid_position = [row, column]
+    return grid_position
+
+def convert_grid_position_to_coordinates(conv_grid_position: list):
+    row, column = conv_grid_position
+    coordinate_x = int(BOARD_LINE_TOTAL_WIDTH + BOARD_TILE_SIZE_PX / 2 + BOARD_TILE_SIZE_PX * (column))
+    coordinate_y = int(BOARD_LINE_TOTAL_WIDTH + BOARD_TILE_SIZE_PX / 2 + BOARD_TILE_SIZE_PX * (row))
+    coordinates = [coordinate_x, coordinate_y]
+    return coordinates
